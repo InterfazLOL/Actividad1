876913ad027e4df4ee13de32484c8bc67eae2169
Josue <josuenunezprada@gmail.com>
2020-05-16 12:06:53 -1000

    First commit

---
+++ src/App/App.java
@@ -0,0 +1,9 @@
+import vistaPrincipal.VistaPrincipalTemplate;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        VistaPrincipalTemplate visPrincipal = new VistaPrincipalTemplate();
+        
+    }
+}
